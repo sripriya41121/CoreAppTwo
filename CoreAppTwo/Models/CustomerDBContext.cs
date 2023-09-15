e3a86da27601f17f2731728846cdce1149e3c556
SriPriya <sripriyaannepu@gmail.com>
2023-09-15 09:26:16 -0700

    Initial Commit

---
+++ CoreAppTwo/Models/CustomerDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreAppTwo.Models
+{
+    public class CustomerDBContext: DbContext
+    {
+        public CustomerDBContext(DbContextOptions<CustomerDBContext> options)
+        : base(options) 
+
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ CoreAppTwo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreAppTwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
